578623e344d89f937feaae79cf43f0fd74144e5b
MarcoDelpimme <Marcodelimme@outlook.com>
2024-02-01 15:52:58 +0100

    first commit

---
+++ assets/index.js
@@ -0,0 +1,28 @@
+const newCell = (cell) => {
+  const containerCell = document.getElementById("container-tombola");
+
+  for (let i = 1; i < cell; i++) {
+    const cellDiv = document.createElement("div"); //sto creando il div
+    cellDiv.className = "cella";
+
+    const cellH3 = document.createElement("h3");
+    cellH3.innerText = i;
+
+    cellDiv.appendChild(cellH3);
+    containerCell.appendChild(cellDiv);
+  }
+};
+
+newCell(77);
+
+const button = document.getElementById("button1");
+button.onclick = function () {
+  const cells = document.querySelectorAll(".cella");
+  const randomNum = Math.floor(Math.random() * cells.length);
+  const randomCell = cells[randomNum];
+  const newNum = randomCell;
+  console.log(randomCell);
+  randomCell.style.backgroundColor = "red";
+
+  if (randomCell === randomCell) return newNum;
+};
