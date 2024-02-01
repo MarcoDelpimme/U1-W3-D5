858952e352a7c00175c5ab17ba1108ee0fef5955
MarcoDelpimme <Marcodelimme@outlook.com>
2024-02-01 17:38:06 +0100

    commit2

--- assets/index.js
+++ assets/index.js
@@ -20,18 +20,22 @@ button.onclick = function () {
   const cells = document.querySelectorAll(".cella");
   const randomNum = Math.floor(Math.random() * cells.length);
   const randomCell = cells[randomNum];
-  //   const newNum = randomCell; test
+  //   const newNum = randomCell;
   console.log(randomCell);
   randomCell.style.backgroundColor = "red";
 };
 
-const button2 = domcument.getElementById("button2");
+//crea nuove tabelle
+
+const button2 = document.getElementById("button2");
 button2.onclick = function () {
   const tableContainer = document.getElementById("tablecontainer");
-  const newDiv = document.createElement("div");
-  newDiv.className = "playertablediv";
-  const newH3 = document.createElement("h3");
-  newH3.innerText = i;
-  newDiv.appendChild(newH3);
-  tableContainer.appendChild(newDiv);
+  for (let i = 1; i <= 27; i++) {
+    const newDiv = document.createElement("div");
+    newDiv.className = "playertablediv";
+    const newH3 = document.createElement("h3");
+    newH3.innerText = i;
+    newDiv.appendChild(newH3);
+    tableContainer.appendChild(newDiv);
+  }
 };
