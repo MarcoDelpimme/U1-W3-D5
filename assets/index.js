ec8f18b6700f62cb787e82fe36f1a9b22e17118f
MarcoDelpimme <Marcodelimme@outlook.com>
2024-02-01 16:44:33 +0100

    littlemodify

--- assets/index.js
+++ assets/index.js
@@ -20,9 +20,18 @@ button.onclick = function () {
   const cells = document.querySelectorAll(".cella");
   const randomNum = Math.floor(Math.random() * cells.length);
   const randomCell = cells[randomNum];
-  const newNum = randomCell;
+  //   const newNum = randomCell; test
   console.log(randomCell);
   randomCell.style.backgroundColor = "red";
+};
 
-  if (randomCell === randomCell) return newNum;
+const button2 = domcument.getElementById("button2");
+button2.onclick = function () {
+  const tableContainer = document.getElementById("tablecontainer");
+  const newDiv = document.createElement("div");
+  newDiv.className = "playertablediv";
+  const newH3 = document.createElement("h3");
+  newH3.innerText = i;
+  newDiv.appendChild(newH3);
+  tableContainer.appendChild(newDiv);
 };
